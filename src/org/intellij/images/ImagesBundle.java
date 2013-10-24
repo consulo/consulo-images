072b66dbb603b75716fd860393d2fe9322753448
VISTALL <vistall.valeriy@gmail.com>
2013-10-24 11:23:09 +0300

    @Bundle

--- src/org/intellij/images/ImagesBundle.java
+++ src/org/intellij/images/ImagesBundle.java
@@ -15,36 +15,11 @@
  */
 package org.intellij.images;
 
-import com.intellij.CommonBundle;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.PropertyKey;
-
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
+import org.consulo.lombok.annotations.Bundle;
 
 /**
  * @author max
  */
+@Bundle("org.intellij.images.ImagesBundle")
 public class ImagesBundle {
-  private static Reference<ResourceBundle> ourBundle;
-
-  @NonNls private static final String BUNDLE = "org.intellij.images.ImagesBundle";
-
-  private ImagesBundle() {
-  }
-
-  public static String message(@PropertyKey(resourceBundle = BUNDLE)String key, Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = null;
-    if (ourBundle != null) bundle = ourBundle.get();
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<ResourceBundle>(bundle);
-    }
-    return bundle;
-  }
 }
