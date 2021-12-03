d31f4893e0cd6ffe070c9c4e5c7916608c6fac22
VISTALL <vistall.valeriy@gmail.com>
2021-12-03 03:09:47 +0300

    update image icons

--- src/main/java/icons/ImagesIcons.java
+++ src/main/java/icons/ImagesIcons.java
@@ -6,11 +6,9 @@ import consulo.ui.image.Image;
 @Deprecated
 public class ImagesIcons
 {
-	public static final Image EditExternaly = ImagesIconGroup.EditExternaly();
 	public static final Image ImagesFileType = ImagesIconGroup.ImagesFileType();
 	public static final Image ThumbnailBlank = ImagesIconGroup.ThumbnailBlank();
 	public static final Image ThumbnailDirectory = ImagesIconGroup.ThumbnailDirectory();
 	public static final Image ThumbnailToolWindow = ImagesIconGroup.ThumbnailToolWindow();
-	public static final Image ToggleGrid = ImagesIconGroup.ToggleGrid();
 	public static final Image ToggleTransparencyChessboard = ImagesIconGroup.ToggleTransparencyChessboard();
 }
