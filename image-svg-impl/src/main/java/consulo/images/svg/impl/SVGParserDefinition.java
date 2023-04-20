3bd35448d60bb6a32a8ffb201e9aab3876407301
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-04-20 18:48:16 +0400

    add missed #getLanguage to SVGParserDefinition

--- image-svg-impl/src/main/java/consulo/images/svg/impl/SVGParserDefinition.java
+++ image-svg-impl/src/main/java/consulo/images/svg/impl/SVGParserDefinition.java
@@ -2,6 +2,7 @@ package consulo.images.svg.impl;
 
 import consulo.annotation.component.ExtensionImpl;
 import consulo.images.svg.SVGLanguage;
+import consulo.language.Language;
 import consulo.language.ast.IFileElementType;
 import consulo.language.file.FileViewProvider;
 import consulo.language.psi.PsiFile;
@@ -15,18 +16,28 @@ import javax.annotation.Nonnull;
  * @since 10/12/2021
  */
 @ExtensionImpl
-public class SVGParserDefinition extends XMLParserDefinition {
-  private static final IFileElementType SVG_FILE = new IFileElementType(SVGLanguage.INSTANCE);
+public class SVGParserDefinition extends XMLParserDefinition
+{
+	private static final IFileElementType SVG_FILE = new IFileElementType(SVGLanguage.INSTANCE);
 
-  @Nonnull
-  @Override
-  public PsiFile createFile(FileViewProvider viewProvider) {
-    return new XmlFileImpl(viewProvider, SVG_FILE);
-  }
+	@Nonnull
+	@Override
+	public PsiFile createFile(FileViewProvider viewProvider)
+	{
+		return new XmlFileImpl(viewProvider, SVG_FILE);
+	}
 
-  @Nonnull
-  @Override
-  public IFileElementType getFileNodeType() {
-    return SVG_FILE;
-  }
+	@Nonnull
+	@Override
+	public IFileElementType getFileNodeType()
+	{
+		return SVG_FILE;
+	}
+
+	@Nonnull
+	@Override
+	public Language getLanguage()
+	{
+		return SVGLanguage.INSTANCE;
+	}
 }
