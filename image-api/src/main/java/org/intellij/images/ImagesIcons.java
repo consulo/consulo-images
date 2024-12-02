2338db6f8f503571a0637c352b01a8668eea7bcd
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-12-02 16:01:21 +0400

    update icons

--- image-api/src/main/java/org/intellij/images/ImagesIcons.java
+++ image-api/src/main/java/org/intellij/images/ImagesIcons.java
@@ -1,13 +1,14 @@
 package org.intellij.images;
 
 import consulo.images.icon.ImagesIconGroup;
+import consulo.platform.base.icon.PlatformIconGroup;
 import consulo.ui.image.Image;
 
 @Deprecated
 public class ImagesIcons {
   public static final Image ImagesFileType = ImagesIconGroup.imagesfiletype();
-  public static final Image ThumbnailBlank = ImagesIconGroup.thumbnailblank();
-  public static final Image ThumbnailDirectory = ImagesIconGroup.thumbnaildirectory();
+  public static final Image ThumbnailBlank = Image.empty(Image.DEFAULT_ICON_SIZE);
+  public static final Image ThumbnailDirectory = PlatformIconGroup.nodesFolder();
   public static final Image ThumbnailToolWindow = ImagesIconGroup.thumbnailtoolwindow();
   public static final Image ToggleTransparencyChessboard = ImagesIconGroup.toggletransparencychessboard();
 }
