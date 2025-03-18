99389698d0641c8f26aaa4adb8f11b72982d93f8
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-03-18 20:16:43 +0400

    migrate to extending scheme from code not xml

--- plugin/src/main/java/consulo/images/impl/colorScheme/DarkAdditionalTextAttributesProvider.java
+++ plugin/src/main/java/consulo/images/impl/colorScheme/DarkAdditionalTextAttributesProvider.java
@@ -1,8 +1,10 @@
 package consulo.images.impl.colorScheme;
 
 import consulo.annotation.component.ExtensionImpl;
-import consulo.colorScheme.AdditionalTextAttributesProvider;
+import consulo.colorScheme.EditorColorSchemeExtender;
 import consulo.colorScheme.EditorColorsScheme;
+import consulo.images.ImageColorKeys;
+import consulo.ui.color.RGBColor;
 import jakarta.annotation.Nonnull;
 
 /**
@@ -10,10 +12,17 @@ import jakarta.annotation.Nonnull;
  * @since 06/06/2024
  */
 @ExtensionImpl
-public class DarkAdditionalTextAttributesProvider implements AdditionalTextAttributesProvider {
+public class DarkAdditionalTextAttributesProvider implements EditorColorSchemeExtender {
+    @Override
+    public void extend(Builder builder) {
+        builder.add(ImageColorKeys.GRID_LINE_COLOR_KEY, new RGBColor(0x00, 0x99, 0xE5));
+        builder.add(ImageColorKeys.CHESSBOARD_BLACK_CELL_COLOR_KEY, new RGBColor(0x3B, 0x3B, 0x3B));
+        builder.add(ImageColorKeys.CHESSBOARD_WHITE_CELL_COLOR_KEY, new RGBColor(0x50, 0x50, 0x50));
+    }
+
     @Nonnull
     @Override
-    public String getColorSchemeName() {
+    public String getColorSchemeId() {
         return EditorColorsScheme.DARCULA_SCHEME_NAME;
     }
 }
--- plugin/src/main/java/consulo/images/impl/colorScheme/LightAdditionalTextAttributesProvider.java
+++ plugin/src/main/java/consulo/images/impl/colorScheme/LightAdditionalTextAttributesProvider.java
@@ -1,8 +1,10 @@
 package consulo.images.impl.colorScheme;
 
 import consulo.annotation.component.ExtensionImpl;
-import consulo.colorScheme.AdditionalTextAttributesProvider;
+import consulo.colorScheme.EditorColorSchemeExtender;
 import consulo.colorScheme.EditorColorsScheme;
+import consulo.images.ImageColorKeys;
+import consulo.ui.color.RGBColor;
 import jakarta.annotation.Nonnull;
 
 /**
@@ -10,10 +12,17 @@ import jakarta.annotation.Nonnull;
  * @since 06/06/2024
  */
 @ExtensionImpl
-public class LightAdditionalTextAttributesProvider implements AdditionalTextAttributesProvider {
+public class LightAdditionalTextAttributesProvider implements EditorColorSchemeExtender {
     @Nonnull
     @Override
-    public String getColorSchemeName() {
+    public String getColorSchemeId() {
         return EditorColorsScheme.DEFAULT_SCHEME_NAME;
+    }
+
+    @Override
+    public void extend(Builder builder) {
+        builder.add(ImageColorKeys.GRID_LINE_COLOR_KEY, new RGBColor(0x00, 0x99, 0xE5));
+        builder.add(ImageColorKeys.CHESSBOARD_BLACK_CELL_COLOR_KEY, new RGBColor(0xEB, 0xEB, 0xEB));
+        builder.add(ImageColorKeys.CHESSBOARD_WHITE_CELL_COLOR_KEY, new RGBColor(0xFF, 0xFF, 0xFF));
     }
 }
